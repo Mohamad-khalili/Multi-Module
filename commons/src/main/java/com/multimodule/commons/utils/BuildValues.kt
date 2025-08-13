9481ff732dfc5e6573f0163ca1df5e810a484b2a
Mohamad khalili <mohamad.khalili0605@gmail.com>
2025-08-13 16:56:04 -0700

    Add identity api It uses for authentication

--- commons/src/main/java/com/multimodule/commons/utils/BuildValues.kt
+++ commons/src/main/java/com/multimodule/commons/utils/BuildValues.kt
@@ -3,7 +3,8 @@ package com.multimodule.commons.utils
 import com.multimodule.commons.BuildConfig
 
 object BuildValues {
-    fun cdnHostAddress() : String = BuildConfig.CDN_HOST
     fun versionName(): String = BuildConfig.VERSION_NAME
-    fun apiHostAddress(): String = BuildConfig.CDN_HOST
+    fun cdnHostAddress() : String = BuildConfig.CDN_HOST
+    fun baseHostAddress(): String = BuildConfig.BASE_HOST
+    fun identityHostAddress(): String = BuildConfig.IDENTITY_HOST
 }
